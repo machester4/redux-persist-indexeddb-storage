c1593fb13624a1b5221f4e3c1e92709c6fa7fe7e
Michael P <mpintos@lithium.com.uy>
2019-12-04 09:49:20 -0300

    Refactor - Test added

---
+++ __tests__/test.js
@@ -0,0 +1,13 @@
+import storage from "../";
+
+it("Save Data", () => {
+  const db = storage("myDb");
+  db.setItem("item", "test");
+  return expect(db.getItem("item")).resolves.toBe("test");
+});
+
+it("Remove Data", async () => {
+  const db = storage("myDb");
+  await db.removeItem("item");
+  return expect(db.getItem("item")).resolves.toBe(null);
+});
--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-import localForage from "localforage";
-
-/**
- * Persist you redux state using IndexedDB
- * @param {string} dbName - IndexedDB database name
- */
-function storage(dbName) {
-  const db = localForage.createInstance({
-    name: dbName
-  });
-  return {
-    db,
-    getItem: db.getItem,
-    setItem: db.setItem
-  };
-}
-
-export default storage;
+export { default } from "./lib";
